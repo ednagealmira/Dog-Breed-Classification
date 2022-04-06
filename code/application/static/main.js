df8c742aa4d61f4d1a907f4bc4d6ec4293d127b3
Ednagea Almira <ednagea.almira@gmail.com>
2022-04-06 10:54:58 +0700

    add class to main.js

--- code/application/static/main.js
+++ code/application/static/main.js
@@ -1,19 +1,25 @@
-let model;
-(async function () {
-    model = await tf.loadLayersModel('http://localhost:81/tfjs-model/MobileNet5/model.json');
-    $(".progress-bar").hide();
-})();
+class Main {
+    start() {
+        let model;
+        (async function () {
+            model = await tf.loadLayersModel('http://localhost:81/tfjs-model/MobileNet5/model.json');
+            $(".progress-bar").hide();
+        })();
+        
+        $("#image-selector").change(function () {
+            let imageSelector = new ImageSelector("#image-selector");
+            imageSelector.insertImage();
+        });
+        
+        $("#predict-button").click(function () {
+            let CNN = new Cnn(model);
+            let image = $("#selected-image").get(0);
+            CNN.predict(image);
+        });
+    }
+}
 
-$("#image-selector").change(function () {
-    let imageSelector = new ImageSelector("#image-selector");
-    imageSelector.insertImage();
-});
-
-$("#predict-button").click(function () {
-    let CNN = new Cnn(model);
-    let image = $("#selected-image").get(0);
-    CNN.predict(image);
-});
+new Main().start();
 
 // $("#image-selector").change(function () {
 //     let reader = new FileReader();
